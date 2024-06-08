2b25d9f20a5c2fcbe6326bb7a1a7788ef4e075e2
Malleswari578 <malleswari.veeramosu@gmail.com>
2024-06-08 15:00:30 -0400

    'myintialsetup'

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import React, { useState } from "react";
+import { get } from "aws-amplify/api";
+
+const myAPI = "apid357c825";
+const path = "/customers";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
+
+  //Function to fetch from our backend and update customers array
+  async function getCustomer(e) {
+    let customerId = e.input;
+    const restOperation = get({
+      apiName: myAPI,
+      path: path + "/" + customerId,
+    });
+    const { body } = await restOperation.response;
+    const response = await body.json();
+    console.log(response);
+    let newCustomers = [...customers];
+    newCustomers.push(response);
+    setCustomers(newCustomers);
+  }
+
+  return (
+    <div className="App">
+      <h1>Super Simple React App</h1>
+      <div>
+        <input
+          placeholder="customer id"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <br />
+      <button onClick={() => getCustomer({ input })}>
+        Get Customer From Backend
+      </button>
+
+      <h2 style={{ visibility: customers.length > 0 ? "visible" : "hidden" }}>
+        Response
+      </h2>
+      {customers.map((thisCustomer, index) => {
+        return (
+          <div key={thisCustomer.customerId}>
+            <span>
+              <b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>
+              : {thisCustomer.customerName}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
